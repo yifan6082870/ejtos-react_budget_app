e733b803bdcfd2c386dce3dc46584609f1b7c067
yifan6082870 <yifan6082870@gmail.com>
2024-01-05 20:07:51 -0500

    Updated

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,25 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { dispatch, budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        if(event.target.value > 2200) {
+            alert("You cannot exceed 20000 budget");
+        } else {
+            setNewBudget(parseInt(event.target.value));
+            dispatch({
+                type: "SET_BUDGET",
+                payload: parseInt(event.target.value),
+            });
+        }
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
